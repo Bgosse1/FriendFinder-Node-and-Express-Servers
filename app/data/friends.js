19a6fd687e7a88d391e5907b948b3abe7b077edf
bryant.gossett@gmail.com <5419632+Bgosse1@users.noreply.github.com>
2019-01-01 16:48:14 -0500

    added friends data

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Snagglepuss',
+		photo: 'https://en.wikipedia.org/wiki/Snagglepuss#/media/File:Snagglepuss.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Yogi Bear',
+		photo: 'https://en.wikipedia.org/wiki/Yogi_Bear#/media/File:Yogi_Bear_Yogi_Bear.png',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Scooby Doo',
+		photo: 'https://www.google.com/imgres?imgurl=https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Scooby-Doo.png/150px-Scooby-Doo.png&imgrefurl=https://en.wikipedia.org/wiki/Scooby-Doo_(character)&h=287&w=150&tbnid=3JbksVmbm5X7KM:&q=Scooby-Doo+(character)&tbnh=186&tbnw=97&usg=AI4_-kR_1dpDrSrfg6TnXAjd5kT99s9DdA&vet=12ahUKEwiI7IKzxM3fAhUJm-AKHV5_D18Q_B0wHnoECAEQBg..i&docid=ocNdwjvrtQFz6M&itg=1&client=firefox-b-1-ab&sa=X&ved=2ahUKEwiI7IKzxM3fAhUJm-AKHV5_D18Q_B0wHnoECAEQBg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Muttley',
+		photo: 'https://en.wikipedia.org/wiki/Muttley#/media/File:Muttley-picture.gif',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Boo-Boo',
+		photo: 'https://en.wikipedia.org/wiki/Boo-Boo_Bear#/media/File:Boo-Boo_Bear.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Pebbles Flinstone',
+		photo: 'https://en.wikipedia.org/wiki/Pebbles_Flintstone#/media/File:Pebbles_Flintstone.png',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Atom Ant',
+		photo: 'https://en.wikipedia.org/wiki/Atom_Ant#/media/File:Atom_Ant.png',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Barney Rubble',
+		photo: 'https://en.wikipedia.org/wiki/Barney_Rubble#/media/File:Barney_Rubble.png',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Dick Dastardly',
+		photo: 'https://en.wikipedia.org/wiki/Dick_Dastardly#/media/File:Dickdastardly.gif',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Huckleberry Hound',
+		photo: 'https://en.wikipedia.org/wiki/Huckleberry_Hound#/media/File:Huckleberry_Hound.png',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
